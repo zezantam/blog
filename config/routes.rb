ae4bf953f65ae28be018dba95e11d834f6b67d5c
Zezan Tam <tam.zezan@gmail.com>
2014-02-17 22:02:23 +1100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Blog::Application.routes.draw do
+
+  resources :posts
+
+  root 'welcome#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +59,3 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
